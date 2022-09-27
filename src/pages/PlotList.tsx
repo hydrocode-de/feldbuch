4ae8bec43b956483d8b27c91c9586064174a67fb
Mirko Mälicke <mirko@hydrocode.de>
2022-09-27 10:18:03 +0200

    improved start page

--- src/pages/PlotList.tsx
+++ src/pages/PlotList.tsx
@@ -1,12 +1,15 @@
-import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonMenuToggle, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonMenuToggle, IonPage, IonRow, IonText, IonTitle, IonToolbar } from "@ionic/react";
 import LoginButton from "../components/LoginButton";
 import MainMenu from "../components/MainMenu";
 import PlotListItem from "../components/PlotListItem";
+import SyncButton from "../components/SyncButton";
+import { useAuth } from "../supabase/auth";
 import { useFeldbuch } from "../supabase/feldbuch"
 
 const PlotList: React.FC = () => {
     // use the Feldbuch context
     const { plots } = useFeldbuch();
+    const { user } = useAuth();
 
     return (
         <>
@@ -32,9 +35,31 @@ const PlotList: React.FC = () => {
                             </IonToolbar>
                         </IonHeader>
 
-                        <IonList>
-                            { plots.map((plot, idx) => <PlotListItem key={idx} plot={plot} />) }
-                        </IonList>
+                        { plots.length > 0 ? (
+                            <IonList>
+                                { plots.map((plot, idx) => <PlotListItem key={idx} plot={plot} />) }
+                            </IonList>
+                        ) : (
+                            <IonGrid style={{height: '100%'}}>
+                                <IonCol size="12">
+                                    <IonRow className="ion-justify-content-center">
+                                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                                    <IonLabel color="warning" className="ion-text-wrap">
+                                        Keine lokalen Daten gefunden.
+                                        { user ? 'Versuche dich mit der Datenbank zu synchonisieren' : 'Zum Synchronisieren musst du dich einloggen.'}
+                                    </IonLabel>
+                                    { user ? (
+                                        <SyncButton expand="block" fill="outline" />
+                                    ) : (
+                                        <IonButton color="primary" fill="outline" routerLink="/login" routerDirection="forward">zum Login</IonButton>
+                                    )}
+                                    </div>
+                                    </IonRow>
+                                </IonCol>
+                            </IonGrid>
+                            
+                        ) }
+                        
 
                     </IonContent>
                 </IonHeader>
